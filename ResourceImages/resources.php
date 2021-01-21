ea585e0a2fd3e7b1c4aafaa218b53999f42a874c
rentiger1 <saka_mora@hotmail.com>
2021-01-20 20:12:58 -0700

    Add files via upload

---
+++ ResourceImages/resources.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Welcome to Alliance Larp Utah</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://csis2440.app/CSIS2470/NewLarp/quickstart/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    <!-- font awesome icons -->
+    <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="https://csis2440.app/CSIS2470/NewLarp/quickstart/css/business-casual.min.css" rel="stylesheet">
+    <!-- jquery  -->
+
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
+  <!-- drop box dependancies  -->
+  <script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/4.0.13/Dropbox-sdk.min.js"></script>
+
+<script>
+
+
+
+
+</script>
+<style>
+.cta-inner {
+  position: relative;
+  padding: 3rem;
+  margin: 0.5rem;
+  background-color: rgba(255, 255, 255, 0.85);
+}
+
+.cta-inner:before {
+  border-radius: 0.5rem;
+  position: absolute;
+  top: -0.5rem;
+  bottom: -0.5rem;
+  left: -0.5rem;
+  right: -0.5rem;
+  border: 0.25rem solid rgba(255, 255, 255, 0.85);
+}
+</style>
+  </head>
+
+  <body>
+
+    <h1 class="site-heading text-center text-white d-none d-lg-block">
+      <span class="site-heading-upper text-primary mb-3">Welcome to Alliance Larp Utah</span>
+      <span class="site-heading-lower">A Prince's Calling</span>
+
+    </h1>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="https://csis2440.app/CSIS2470/NewLarp/quickstart/index.html">
+                <i class="fa fa-home"> </i>Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="https://csis2440.app/CSIS2470/NewLarp/quickstart/about.html">About</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="https://csis2440.app/CSIS2470/NewLarp/quickstart/contact.html">
+                Contact Us</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="https://csis2440.app/CSIS2470/NewLarp/quickstart/donate.html">
+                <i class="fa fa-currency"></i>Donate</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="https://csis2440.app/CSIS2470/NewLarp/quickstart/sponsors.html">Our Sponsors</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="row ">
+      <div class="col-md-4">
+      <section class="page-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-9 mx-auto">
+              <div class="cta-inner text-center rounded">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-upper">Locations</span>
+                </h2>
+                <div id="primary-locations">
+                <h3 id="loc-1"> Hamal </h3>
+                <h3 id="loc-4"> Litimore </h3>
+                <div class="loc-4" hidden>
+                  <hr>
+                <ul>
+                  <li id="loc-4-1">Yannic</li>
+                </ul>
+              </div>
+                <h3 id="loc-2"> Undermeer </h3>
+                <h3 id="loc-3"> Antadain </h3>
+                <h3 id="loc-5"> Damoria </h3>
+              </div> <!--primary locations end -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+      <div class="col-md-8">
+    <section class="page-section" >
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <div class="cta-inner text-center rounded">
+              <h2 class="section-heading mb-4">
+                <span hidden id="return" class="section-heading-upper">
+                <a href="resources.php">
+                  <i class="fa fa-arrow-left"> </i> Return Back
+                </a>
+                  </span>
+                <span class="section-heading-lower">Our Realm</span>
+              </h2>
+              <div id="full-map">
+                <img id="image-swap" src="ResourceImages/fullMap.jpeg" alt="" usemap="#Map-0" />
+<map name="Map-0" id="Map-0">
+    <area alt="" title="" href="#" class="loc" id="1" shape="poly" coords="56,142,72,62,54,30,26,12,1,2,3,140,2,143" />
+    <area alt="" title="" href="#" class="loc" id="2" shape="poly" coords="193,2,199,24,213,34,234,41,280,40,301,43,314,30,325,17,329,5,327,2" />
+    <area alt="" title="" href="#" class="loc" id="3" shape="poly" coords="75,0,84,18,117,29,143,45,183,47,193,44,196,26,192,8,187,1" />
+    <area alt="" title="" href="#" class="loc" id="4" shape="poly" coords="197,34,213,60,212,79,218,103,212,115,225,123,250,119,261,117,271,114,257,59,254,43" />
+    <area alt="" title="" href="#" class="loc" id="5"shape="poly"  coords="274,114,263,123,253,141,265,156,270,170,276,178,323,169,339,173,327,146" />
+    <!-- <area alt="" title="" href="#" class="loc" id="6" shape="poly" coords="201,120,241,138,251,165,253,178,160,178,156,137" />
+    <area alt="" title="" href="#" class="loc" id="7" shape="poly" coords="70,147,137,142,152,164,151,178,42,178" /> -->
+</map>
+</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div> <!--end of col -->
+</div><!--end of row -->
+  </div>
+    <footer class="footer text-faded text-center py-5">
+      <div class="container">
+        <p class="m-0 small">Copyright &copy; 2018 Alliance Larp Utah - All Rights Reserved.</p>
+      </div>
+    </footer>
+    <script>
+    $('#primary-locations').fadeOut();
+    // below is the fading for the map and legend
+    //console.log("does this work");
+    $('#full-map').hover(function(){
+        $('#primary-locations').fadeIn("slow");
+    }//,
+    // function(){
+    //     $('#primary-locations').fadeOut();
+    // }
+    );
+
+    $('.loc').hover(function(){
+      var id = $(this).attr('id');
+      //console.log(id);
+        $('#loc-'+id).addClass('text-primary');
+    },
+    function(){
+      var id = $(this).attr('id');
+        $('#loc-'+id).removeClass('text-primary');
+    });
+
+    $('.loc').click(function(){
+      var id = $(this).attr('id');
+      var newImage = "ResourceImages/img-" + id + ".jpeg";
+      $('#image-swap').attr('src', newImage);
+      $('#image-swap').attr('usemap', "#Map-"+id);
+      $('.loc-' + id).removeAttr('hidden');
+      $('#return').removeAttr('hidden');
+
+    });
+    </script>
+  </body>
+
+</html>
---
+++ partials/navbar.php
@@ -0,0 +1,40 @@
+<?php 
+
+echo <<< TEXT
+<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="index.php">
+                <i class="fa fa-home"> </i>Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="about.html">About</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="contact.html">
+                Contact Us</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="donate.html">
+                <i class="fa fa-currency"></i>Donate</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="sponsors.html">Our Sponsors</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+TEXT;
+        
+
+?>
